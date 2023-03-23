539a56f8842509a301f13f073da530d8441728f6
bbairdgen <bbairdgen@gmail.com>
2023-03-22 20:41:59 -0600

    adding models

--- models/index.js
+++ models/index.js
@@ -1,1 +1,30 @@
 // create module export
+const User = require('./User')
+const Autocorrect = require('./Autocorrect')
+const Total = require('./Total')
+
+User.hasMany(Autocorrect, {
+    foreignKey: 'user_id'
+});
+
+Autocorrect.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Total, {
+    foreignKey: 'user_id'
+});
+
+Total.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Autocorrect.hasMany(Total, {
+    foreignKey: 'autocorrect_id'
+});
+
+Total.belongsTo(Autocorrect, {
+    foreignKey: 'autocorrect_id'
+});
+
+module.exports = { User, Total, Autocorrect }
