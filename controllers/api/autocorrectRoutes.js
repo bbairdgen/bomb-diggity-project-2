a70f3742029eaf0d23ea6d397abce1d8b047f25d
jerk-store <taran.leu@outlook.com>
2023-03-23 19:19:25 -0600

    adding autocorrectroutesss

--- controllers/api/autocorrectRoutes.js
+++ controllers/api/autocorrectRoutes.js
@@ -1,20 +1,18 @@
 //to do
 const router = require("express").Router();
-const { Autocorrect } = require("../models");
+const { Autocorrect } = require("../../models");
 const withAuth = require("../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
-    try {
-        const newAutocorrect = await.Autocorrect.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
-        res.status(200).json(newAutocorrect);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newAutocorrect = await Autocorrect.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newAutocorrect);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-
-
 
 module.exports = router;
