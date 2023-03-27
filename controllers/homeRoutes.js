86499c40e7277afc76903198a6945a7194c23c19
jerk-store <taran.leu@outlook.com>
2023-03-26 20:08:15 -0600

    added get by id for autocorrects table, played with materialize a little, trying to get homepage handlebars to display everything in autocorrects table but haven't cracked it yet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const { Autocorrect, User, Total } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.render('homepage', {
+    res.render("homepage", {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -12,33 +12,31 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   try {
-
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Autocorrect }, { model: Total }]
-    })
+      attributes: { exclude: ["password"] },
+      include: [{ model: Autocorrect }, { model: Total }],
+    });
 
-    const user = userData.get({ plain: true})
+    const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("profile", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
+});
 
-})
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login')
-})
+  res.render("login");
+});
 
 module.exports = router;
